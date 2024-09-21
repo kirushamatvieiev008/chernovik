1b77d7580d027c180a8cbf8732882431f6941c45
Кірюша <mkiril087@gmail.com>
2024-09-21 09:59:31 +0300

    gh

---
+++ undex1.js
@@ -0,0 +1,18 @@
+// const clients = ['Mango', 'Poly', 'Ajax', 'tyurtyu', 'color'];
+// console.log(clients[1]);
+// clients[0] = 'Alex';
+// console.log(clients);
+// clients[4799999998
+// ] = 'colorado';
+// console.log(clients);
+
+// const clients = ['Mango', 'Ajax', 'Poly'];
+// for (let i = 0; i < clients.length; i += 1) {
+//   console.log('Logging clients: ', clients[i]);
+// }
+
+const numbers = [];
+for (let i = 0; i < 30; i += 1) {
+  numbers.push(`label-${i}`);
+}
+console.log('numbers: ', numbers);
